22cc67d27a447c1c5b9f08cae69e02a33699a175
Kaspar Valk <valk.kaspar@gmail.com>
2018-05-04 18:55:59 +0300

    Initial commit

---
+++ desktop/src/com/oopgame/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.oopgame.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.oopgame.game.OOPGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new OOPGame(), config);
+	}
+}
