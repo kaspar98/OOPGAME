cd3b2f033d565a706b37aac72e237ec0cd5f8564
Oliver <o.vainumae@gmail.com>
2018-05-07 19:22:57 +0300

    Player kasutab nüüd Rectangle boundsi asemel Sprite'i

--- core/src/com/oopgame/game/Player.java
+++ core/src/com/oopgame/game/Player.java
@@ -3,8 +3,8 @@ package com.oopgame.game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.CircleShape;
@@ -20,7 +20,7 @@ public class Player implements DynamicBodied {
 
     // playeri tekstuur ja tekstuuri ääred jne
     private Texture texture;
-    public Rectangle bounds;
+    public Sprite sprite;
 
     private World world;
     private Body body;
@@ -32,12 +32,11 @@ public class Player implements DynamicBodied {
 
         texture = new Texture(Gdx.files.internal("player_laev.png"));
 
-        // seda Rectangle'i ei pruugi meil varsti üldse vaja minna, mulle tundub
-        bounds = new Rectangle(
+        // loome tekstuuriga tegeleva sprite'i
+        sprite = new Sprite(texture);
+        sprite.setPosition(
                 x - texture.getWidth() / 2f,
-                y - texture.getHeight() / 2f,
-                texture.getWidth(),
-                texture.getHeight()
+                y - texture.getHeight() / 2f
         );
 
 
@@ -75,7 +74,7 @@ public class Player implements DynamicBodied {
         // paremale
         if (Gdx.input.isKeyPressed(Keys.RIGHT)) {
             body.applyForceToCenter(
-                    10* GameInfo.FORCE_MULTIPLIER,
+                    10 * GameInfo.FORCE_MULTIPLIER,
                     0.0f,
                     true
             );
@@ -84,7 +83,7 @@ public class Player implements DynamicBodied {
         // vasakule
         if (Gdx.input.isKeyPressed(Keys.LEFT)) {
             body.applyForceToCenter(
-                    -10* GameInfo.FORCE_MULTIPLIER,
+                    -10 * GameInfo.FORCE_MULTIPLIER,
                     0.0f,
                     true
             );
@@ -94,7 +93,7 @@ public class Player implements DynamicBodied {
         if (Gdx.input.isKeyPressed(Keys.UP)) {
             body.applyForceToCenter(
                     0.0f,
-                    10* GameInfo.FORCE_MULTIPLIER,
+                    10 * GameInfo.FORCE_MULTIPLIER,
                     true
             );
         }
@@ -103,7 +102,7 @@ public class Player implements DynamicBodied {
         if (Gdx.input.isKeyPressed(Keys.DOWN)) {
             body.applyForceToCenter(
                     0.0f,
-                    -10* GameInfo.FORCE_MULTIPLIER,
+                    -10 * GameInfo.FORCE_MULTIPLIER,
                     true
             );
         }
@@ -111,13 +110,16 @@ public class Player implements DynamicBodied {
 
     // topib tekstuuri SpriteBatchile, x ja y boundsi järgi
     public void render(float delta) {
-        game.batch.draw(texture, bounds.x, bounds.y);
-        /*Vector2 asukoht = fixture.getBody().getPosition();
-        game.batch.draw(texture, asukoht.x, asukoht.y);*/
+        // sprite tegeleb nüüd tekstuuri renderimisega ise
+        sprite.draw(game.batch);
     }
 
     public void bodyUpdate() {
-        bounds.setCenter(body.getPosition());
+        // muudab sprite'i keskpunkti asukoht vastavalt keha asukohale
+        sprite.setCenter(body.getPosition().x, body.getPosition().y);
+
+        // muudab sprite'i suunda vastavalt keha suunale
+        sprite.setRotation(MathUtils.radiansToDegrees * (body.getAngle()));
     }
 
     public void dispose() {
