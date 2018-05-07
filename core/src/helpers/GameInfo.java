4578fa38b5366238d105afd84c8ec4e4215948c0
Oliver <o.vainumae@gmail.com>
2018-05-07 14:12:37 +0300

    lisas konstandi box2d jaoks: jõudude kordaja

--- core/src/helpers/GameInfo.java
+++ core/src/helpers/GameInfo.java
@@ -3,4 +3,6 @@ package helpers;
 public class GameInfo {
     public static final int WIDTH = 800;
     public static final int HEIGHT = 480;
+
+    public static final float FORCE_MULTIPLIER = 100000;
 }
