8b4e372ce50a5aec4d1fdcf89d73701c43eabb88
Oliver <o.vainumae@gmail.com>
2018-05-07 14:13:31 +0300

    Lisas box2d füüsikamootori

--- core/src/com/oopgame/game/Player.java
+++ core/src/com/oopgame/game/Player.java
@@ -4,6 +4,15 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.World;
+
+import helpers.GameInfo;
 
 public class Player {
     // viide OOPGame'ile, et SpriteBatch kätte saada ntks
@@ -13,41 +22,98 @@ public class Player {
     private Texture texture;
     public Rectangle bounds;
 
+    private World world;
+    private Body body;
+    private Fixture fixture;
+
     // konstruktor x ja y alusel
-    public Player(OOPGame game, float x, float y) {
+    public Player(OOPGame game, float x, float y, World world) {
         this.game = game;
 
         texture = new Texture(Gdx.files.internal("player_laev.png"));
+
+        // seda Rectangle'i ei pruugi meil varsti üldse vaja minna, mulle tundub
         bounds = new Rectangle(
                 x - texture.getWidth() / 2f,
                 y - texture.getHeight() / 2f,
                 texture.getWidth(),
                 texture.getHeight()
         );
+
+
+        this.world = world;
+
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.type = BodyDef.BodyType.DynamicBody;
+        bodyDef.position.set(x, y);
+
+        // loome Playerile keha
+        body = world.createBody(bodyDef);
+        body.setUserData(bounds);
+
+        // esialgselt katsetan keha kuju ringina
+        // hiljem peaks ta vist olema mingi ristkülik, mille sisse jääb ainult Playeri tekstuuril
+        // nähtav kosmoselaeva põhiosa ning tiivad jääksid välja sellest
+        // siis oleks Playeril rohkem "dodge'imise" ruumi, mingi lisa rahuldustunne :D
+        CircleShape circle = new CircleShape();
+        circle.setRadius(texture.getWidth() / 2f);
+
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.shape = circle;
+        fixtureDef.density = 0.5f;
+        fixtureDef.friction = 0.4f;
+        fixtureDef.restitution = 0.6f;
+
+        fixture = body.createFixture(fixtureDef);
+
+        circle.dispose();
     }
 
     // testimiseks väga lambine inputi jälgimine
     public void inputs() {
+        // iga nupuvajutus rakendab Playeri kehale jõudu antud vektori suunas
+        // paremale
         if (Gdx.input.isKeyPressed(Keys.RIGHT)) {
-            bounds.x += 200 * Gdx.graphics.getDeltaTime();
+            body.applyForceToCenter(
+                    10* GameInfo.FORCE_MULTIPLIER,
+                    0.0f,
+                    true
+            );
         }
 
+        // vasakule
         if (Gdx.input.isKeyPressed(Keys.LEFT)) {
-            bounds.x -= 200 * Gdx.graphics.getDeltaTime();
+            body.applyForceToCenter(
+                    -10* GameInfo.FORCE_MULTIPLIER,
+                    0.0f,
+                    true
+            );
         }
 
+        // üles
         if (Gdx.input.isKeyPressed(Keys.UP)) {
-            bounds.y += 200 * Gdx.graphics.getDeltaTime();
+            body.applyForceToCenter(
+                    0.0f,
+                    10* GameInfo.FORCE_MULTIPLIER,
+                    true
+            );
         }
 
+        // alla
         if (Gdx.input.isKeyPressed(Keys.DOWN)) {
-            bounds.y -= 200 * Gdx.graphics.getDeltaTime();
+            body.applyForceToCenter(
+                    0.0f,
+                    -10* GameInfo.FORCE_MULTIPLIER,
+                    true
+            );
         }
     }
 
     // topib tekstuuri SpriteBatchile, x ja y boundsi järgi
     public void render(float delta) {
         game.batch.draw(texture, bounds.x, bounds.y);
+        /*Vector2 asukoht = fixture.getBody().getPosition();
+        game.batch.draw(texture, asukoht.x, asukoht.y);*/
     }
 
     public void dispose() {
--- core/src/scenes/GameScreen.java
+++ core/src/scenes/GameScreen.java
@@ -5,6 +5,13 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.Box2D;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.oopgame.game.OOPGame;
@@ -21,8 +28,15 @@ public class GameScreen implements Screen {
     private Player player;
     private Texture bg;
 
+    private World world;
+    private Box2DDebugRenderer debugRenderer;
+
     public GameScreen(final OOPGame game) {
         this.game = game;
+
+        Box2D.init();
+
+        world = new World(new Vector2(0, 0), true);
 
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 800, 480);
@@ -33,7 +47,11 @@ public class GameScreen implements Screen {
         // loeme sisse tausta faili
         bg = new Texture(Gdx.files.internal("test_taust.png"));
         // loome Playeri tausta keskele
-        player = new Player(game,bg.getWidth() / 2f, bg.getHeight() / 2f);
+        player = new Player(game, bg.getWidth() / 2f, bg.getHeight() / 2f, world);
+
+        // debug renderer
+        debugRenderer = new Box2DDebugRenderer();
+
     }
 
     @Override
@@ -43,6 +61,9 @@ public class GameScreen implements Screen {
 
     @Override
     public void render(float delta) {
+        // box2d world steps
+        world.step(1 / 60f, 6, 2);
+
         // clear the screen with a dark blue color. The
         // arguments to glClearColor are the red, green
         // blue and alpha component in the range [0,1]
@@ -60,15 +81,42 @@ public class GameScreen implements Screen {
         // coordinate system specified by the camera.
         game.batch.setProjectionMatrix(camera.combined);
 
+
         game.batch.begin();
+
+        // see osa on väga temporary, see katab hetkel ainult Playeri keha värskendamist,
+        // teistel objektidel võivad olla hoopis teised asjad, mide värskendada.
+        // Variant oleks teha igale värskendatavale objektile enda meetod, mille alusel
+        // ta ennast värskendab
+        Array<Body> bodies = new Array<Body>();
+        world.getBodies(bodies);
+
+        // käib for-iga läbi need värskendatavad kehad
+        for (Body b : bodies) {
+            Rectangle e = (Rectangle) b.getUserData();
+
+            if (e != null) {
+                // siia paneks hoopis mingi meetodi kutse
+                e.setCenter(b.getPosition());
+            }
+        }
+
         // tausta lisamine
         game.batch.draw(bg, 0, 0);
+
         // kutsub Playeris playeri renderimise välja
         player.render(delta);
+
         game.batch.end();
+
+
+        // debug camera render
+        debugRenderer.render(world, camera.combined);
 
         // input checks
         player.inputs();
+
+
     }
 
     @Override
