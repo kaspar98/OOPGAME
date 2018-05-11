ea973d3d841112ddec81e47d172205f83a411386
Oliver <o.vainumae@gmail.com>
2018-05-11 17:08:50 +0300

    peaks olema täiesti korras nüüd, võtab playeri kiiruse järgi pikkuse ja suuna ning suurus on parem

--- core/src/com/oopgame/game/DustParticle.java
+++ core/src/com/oopgame/game/DustParticle.java
@@ -4,10 +4,33 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Vector2;
 
+import helpers.GameInfo;
+
 public class DustParticle extends Sprite {
     public DustParticle(Texture texture, Vector2 asukoht) {
         super(texture);
+        setSize(0, GameInfo.SCALING);
+
         setPosition(asukoht.x, asukoht.y);
+        flip(true, false);
+    }
+
+    public void uuenda(Vector2 vel) {
+        setPosition(
+                getX() - vel.x / 120,
+                getY() - vel.y / 120
+        );
+
+        setSize(vel.len() / 12, GameInfo.SCALING);
+        setRotation(vel.angle());
+    }
+
+    public float getCenterX() {
+        return getX() + getWidth() / 2f;
+    }
+
+    public float getCenterY() {
+        return getY() + getHeight() / 2f;
     }
 
     public void setPosition(Vector2 asukoht) {
