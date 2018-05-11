4940aafe2d84a53dbbf4e2cadca309d20296479b
Oliver <o.vainumae@gmail.com>
2018-05-11 17:13:18 +0300

    lisasin hüpotenuusi välja: kaugus ekraani keskelt ekraani nurgani, basically ekraani ümberringjoone raadius

--- core/src/helpers/GameInfo.java
+++ core/src/helpers/GameInfo.java
@@ -1,8 +1,11 @@
 package helpers;
 
+import com.badlogic.gdx.math.Vector2;
+
 public class GameInfo {
     public static final int WIDTH = 800;
     public static final int HEIGHT = 480;
+    public static final float HYPOTENUSE = new Vector2(WIDTH, HEIGHT).len() / 2f;
     public static final float SCALING = 0.1f;
 
     public static final float FORCE_MULTIPLIER = 5000;
