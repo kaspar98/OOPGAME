bfec9b93544667277ecab96aaae0a4f1ebb5772d
Oliver <o.vainumae@gmail.com>
2018-05-11 12:28:00 +0300

    Tegin TempSeina ümber nii, et ta tekitaks 4 seina ümber mänguvälja

--- core/src/com/oopgame/game/TempSein.java
+++ core/src/com/oopgame/game/TempSein.java
@@ -5,22 +5,55 @@ import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.Array;
 
 import helpers.GameInfo;
 
 public class TempSein {
-    Body body;
+    Array<Body> bodies = new Array<Body>();
 
-    public TempSein(World world, float x, float y) {
-        BodyDef wallBodyDef = new BodyDef();
+    public TempSein(World world) {
+        float seinaPaksusPool = 120;
+        float seinaPikkusPool = GameInfo.W_WIDTH / 2f + seinaPaksusPool * 3;
 
-        wallBodyDef.position.set(new Vector2(x, y));
+        float[][] coords = new float[][]{
+                {GameInfo.W_WIDTH / 2f, -seinaPaksusPool * 2},
+                {GameInfo.W_WIDTH / 2f, GameInfo.W_HEIGHT + seinaPaksusPool * 2},
+                {-seinaPaksusPool * 2, GameInfo.W_HEIGHT / 2f},
+                {GameInfo.W_WIDTH + seinaPaksusPool * 2, GameInfo.W_HEIGHT / 2f}
+        };
 
-        body = world.createBody(wallBodyDef);
+        // praegu lükkasin seinad nii paika, et 0,0 koordinaadis saaks ka liikuda,
+        // aga lõpuks võiks selle koodi natuke lihtsamaks teha ja optimiseerida
+        // lõpuks võiks nii äkki teha:
 
-        PolygonShape box = new PolygonShape();
-        box.setAsBox(512 * GameInfo.SCALING, 100 * GameInfo.SCALING);
-        body.createFixture(box, 0);
-        box.dispose();
+        /*float seinaPaksusPool = 120;
+        float seinaPikkusPool = GameInfo.W_WIDTH / 2f + seinaPaksusPool;
+
+        float[][] coords = new float[][]{
+                {GameInfo.W_WIDTH / 2f, 0},
+                {GameInfo.W_WIDTH / 2f, GameInfo.W_HEIGHT},
+                {0, GameInfo.W_HEIGHT / 2f},
+                {GameInfo.W_WIDTH, GameInfo.W_HEIGHT / 2f}
+        };*/
+
+        for (int i = 0; i < 4; i++) {
+            BodyDef bodyDef = new BodyDef();
+
+            bodyDef.position.set(coords[i][0], coords[i][1]);
+
+            Body body = world.createBody(bodyDef);
+
+            PolygonShape box = new PolygonShape();
+            if (i / 2 == 0)
+                box.setAsBox(seinaPikkusPool, seinaPaksusPool);
+            else
+                box.setAsBox(seinaPaksusPool, seinaPikkusPool);
+
+            body.createFixture(box, 0).setSensor(true);
+            box.dispose();
+
+            bodies.add(body);
+        }
     }
 }
