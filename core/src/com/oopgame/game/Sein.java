6a03a68ef2b1ca96cb73332f0f49fc43718deb12
Oliver <o.vainumae@gmail.com>
2018-05-11 13:34:31 +0300

    Üksiku seina jaoks klass

---
+++ core/src/com/oopgame/game/Sein.java
@@ -0,0 +1,32 @@
+package com.oopgame.game;
+
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
+
+public class Sein {
+    private Body body;
+    Fixture fixture;
+    private Vector2 lüke;
+
+    public Sein(World world, float x, float y, float hx, float hy, Vector2 lüke) {
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.position.set(x, y);
+
+        Body body = world.createBody(bodyDef);
+
+        PolygonShape box = new PolygonShape();
+        box.setAsBox(hx, hy);
+
+        fixture = body.createFixture(box, 0);
+        fixture.setSensor(true);
+        fixture.setUserData(this);
+
+        box.dispose();
+
+        this.lüke = lüke;
+    }
+}
