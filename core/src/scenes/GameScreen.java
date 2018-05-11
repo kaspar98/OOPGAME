a9b5c3b2fd53fd5c5c75e354f04d2ea574f61d16
Oliver <o.vainumae@gmail.com>
2018-05-12 00:57:12 +0300

    muutsin tausta värvi 0,0,0 mustaks

--- core/src/scenes/GameScreen.java
+++ core/src/scenes/GameScreen.java
@@ -156,7 +156,7 @@ public class GameScreen implements Screen {
         // arguments to glClearColor are the red, green
         // blue and alpha component in the range [0,1]
         // of the color to be used to clear the screen.
-        Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+        Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
 
