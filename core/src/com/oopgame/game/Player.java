d19790f618098d0659ede138540985744d647918
Oliver <o.vainumae@gmail.com>
2018-05-12 13:02:33 +0300

    Laiendab nüüd BodiedSprite klassi, pole ideaalne, seda tuleb veel kuidagi edasi teha, praegu ei ole konstruktori osa veel päris hea

--- core/src/com/oopgame/game/Player.java
+++ core/src/com/oopgame/game/Player.java
@@ -2,54 +2,31 @@ package com.oopgame.game;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
-import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 
 import helpers.GameInfo;
 
-public class Player implements DynamicBodied {
-    // viide OOPGame'ile, et SpriteBatch kätte saada ntks
-    private OOPGame game;
+public class Player extends BodiedSprite {
+    public Player(float x, float y, World world) {
+        super(new Texture(Gdx.files.internal("player_laev.png")));
 
-    // playeri tekstuur ja tekstuuri ääred jne
-    private Texture texture;
-    public Sprite sprite;
-
-    private World world;
-    public Body body;
-    private Fixture fixture;
-
-    // konstruktor x ja y alusel
-    public Player(OOPGame game, float x, float y, World world) {
-        this.game = game;
-
-        texture = new Texture(Gdx.files.internal("player_laev.png"));
-
-        // loome tekstuuriga tegeleva sprite'i ja seadistame ta vastavalt suurendusele
-        sprite = new Sprite(texture);
-
-        sprite.setSize(
-                texture.getWidth() * GameInfo.SCALING,
-                texture.getHeight() * GameInfo.SCALING
+        setSize(
+                getTexture().getWidth() * GameInfo.SCALING,
+                getTexture().getHeight() * GameInfo.SCALING
         );
 
-        sprite.setOrigin(sprite.getWidth() / 2f, sprite.getHeight() / 2f);
+        setOrigin(getWidth() / 2f, getHeight() / 2f);
 
         // positsiooni sisendi arvutus (keskpunkt) -> (nurgapunkt)
-        sprite.setPosition(
-                x - sprite.getWidth() / 2f,
-                y - sprite.getHeight() / 2f
+        setPosition(
+                x - getWidth() / 2f,
+                y - getHeight() / 2f
         );
-
-
-        this.world = world;
 
         BodyDef bodyDef = new BodyDef();
         bodyDef.type = BodyDef.BodyType.DynamicBody;
@@ -60,7 +37,7 @@ public class Player implements DynamicBodied {
         body.setUserData(this);
 
         PolygonShape box = new PolygonShape();
-        box.setAsBox(sprite.getWidth() * 0.15f, sprite.getHeight() * 0.45f);
+        box.setAsBox(getWidth() * 0.15f, getHeight() * 0.45f);
 
         FixtureDef fixtureDef = new FixtureDef();
         fixtureDef.shape = box;
@@ -114,7 +91,7 @@ public class Player implements DynamicBodied {
             sprite.setRotation((float) Math.toDegrees(Math.atan(touchpadY / touchpadX)) - 90);
         }*/
         if (touchpadVector.len() > 0) {
-            sprite.setRotation(touchpadVector.angle() - 90);
+            setRotation(touchpadVector.angle() - 90);
             body.setTransform(
                     body.getPosition(),
                     (touchpadVector.angle() - 90) * MathUtils.degRad
@@ -124,19 +101,8 @@ public class Player implements DynamicBodied {
         System.out.println(body.getLinearVelocity().len());
     }
 
-    // topib tekstuuri SpriteBatchile, x ja y boundsi järgi
-    public void render(float delta) {
-        // sprite tegeleb nüüd tekstuuri renderimisega ise
-        sprite.draw(game.batch);
-    }
-
-    public void bodyUpdate() {
-        // muudab sprite'i keskpunkti asukoht vastavalt keha asukohale
-        sprite.setCenter(body.getPosition().x, body.getPosition().y);
-    }
-
     public void dispose() {
-        // võtab tekstuuri mälust maha
-        texture.dispose();
+        // võtab spraidiga seotud assetid mälust maha
+        dispose();
     }
 }
