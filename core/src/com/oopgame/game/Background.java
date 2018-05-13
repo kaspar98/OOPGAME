f78e767a3e445b880466de109e5c26fd2586bf57
Oliver <o.vainumae@gmail.com>
2018-05-14 01:50:52 +0300

    tausta parallax nüüd igas suunas sama

--- core/src/com/oopgame/game/Background.java
+++ core/src/com/oopgame/game/Background.java
@@ -7,14 +7,13 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import helpers.GameInfo;
 
 public class Background extends Sprite {
-    float dx = 0;
-    float dy = 0;
+    private float dx = 0;
+    private float dy = 0;
 
-    float cropX;
-    float cropY;
+    private float cropX;
+    private float cropY;
 
-    float parallaxConstantX;
-    float parallaxConstantY;
+    private float parallaxConstantXY;
 
     public Background(String textureName, float cropXY) {
         this(textureName, cropXY, cropXY);
@@ -24,8 +23,8 @@ public class Background extends Sprite {
         super(new Texture(Gdx.files.internal(textureName)));
 
         setSize(
-                getWidth() * GameInfo.SCALING,
-                getHeight() * GameInfo.SCALING
+                getWidth() * GameInfo.CAM_SCALING,
+                getHeight() * GameInfo.CAM_SCALING
         );
 
         setOrigin(getWidth() / 2f, getHeight() / 2f);
@@ -33,14 +32,18 @@ public class Background extends Sprite {
         this.cropX = cropX;
         this.cropY = cropY;
 
-        parallaxConstantX = 1 - (getWidth() - 2 * this.cropX) / GameInfo.W_WIDTH;
-        parallaxConstantY = 1 - (getHeight() - 2 * this.cropY) / GameInfo.W_HEIGHT;
+        parallaxConstantXY =
+                1 - (
+                        getWidth() - 2 * (
+                                this.cropX > this.cropY ? this.cropX : this.cropY
+                        )
+                ) / (GameInfo.W_WIDTH > GameInfo.W_HEIGHT ? GameInfo.W_WIDTH : GameInfo.W_HEIGHT);
     }
 
     public void update(float x, float y) {
         setPosition(
-                dx + x * parallaxConstantX - cropX,
-                dy + y * parallaxConstantY - cropY
+                dx + x * parallaxConstantXY - cropX,
+                dy + y * parallaxConstantXY - cropY
         );
     }
 
