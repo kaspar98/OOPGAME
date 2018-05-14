b1205cc550facf4584822f36e0825a4a3b510a92
Oliver <o.vainumae@gmail.com>
2018-05-14 12:39:47 +0300

    uus klass UI kompassi markerite jaoks

---
+++ core/src/com/oopgame/game/UIMarker.java
@@ -0,0 +1,47 @@
+package com.oopgame.game;
+
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
+
+import helpers.GameInfo;
+
+public class UIMarker extends Sprite {
+    OrthographicCamera camera;
+
+    Vector2 point;
+
+    float radius;
+
+    public UIMarker(Texture texture, OrthographicCamera camera, float radius, Vector2 point) {
+        super(texture);
+
+        this.camera = camera;
+        this.radius = radius;
+        this.point = point;
+
+        setSize(getWidth() * GameInfo.CAM_SCALING,
+                getHeight() * GameInfo.CAM_SCALING);
+
+        setOrigin(getWidth(), getHeight() * 0.5f);
+    }
+
+    public void update() {
+        float angle = new Vector2(
+                point.x - camera.position.x,
+                point.y - camera.position.y
+        ).angle();
+
+        setOriginBasedPosition(
+                camera.position.x + MathUtils.cosDeg(angle) * radius,
+                camera.position.y + MathUtils.sinDeg(angle) * radius);
+
+        setRotation(angle);
+    }
+
+    public void dispose() {
+        getTexture().dispose();
+    }
+}
