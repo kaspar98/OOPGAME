c98e7f9ffdd39f38e8685f602cf3c20f127de14f
Oliver <o.vainumae@gmail.com>
2018-05-14 12:45:28 +0300

    uiManager tekitab nüüd automaatselt markerid, kui kutsuda addMarker() välja

--- core/src/com/oopgame/game/UIManager.java
+++ core/src/com/oopgame/game/UIManager.java
@@ -5,6 +5,8 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Array;
 
 import helpers.GameInfo;
 
@@ -14,6 +16,8 @@ public class UIManager {
     private Player player;
 
     private Sprite compass;
+    private Texture compass_marker_texture;
+    private Array<UIMarker> compass_markers = new Array<UIMarker>();
 
     private float dx = 117 * GameInfo.CAM_SCALING;
 
@@ -34,6 +38,8 @@ public class UIManager {
                 compass.getWidth() * GameInfo.CAM_SCALING,
                 compass.getHeight() * GameInfo.CAM_SCALING);
 
+        compass_marker_texture = new Texture(Gdx.files.internal("ui1_compass_marker1_t.png"));
+
         health = new UIBar(new Texture("ui1_health1_t.png"),
                 player.getMaxHealth(), -dx, 0, camera);
 
@@ -52,12 +58,17 @@ public class UIManager {
                 health_back.getWidth(),
                 health_back.getHeight());
         shield_back.setFlip(true, false);
+
+        addMarker(new Vector2(0, 0));
     }
 
     public void update() {
         compass.setCenter(
                 camera.position.x,
                 camera.position.y);
+
+        for (UIMarker marker : compass_markers)
+            marker.update();
 
         health.update(player.getHealth());
         health_back.setPosition(health.getX(), health.getY());
@@ -71,6 +82,8 @@ public class UIManager {
         health.draw(batch);
         shield.draw(batch);
         compass.draw(batch);
+        for (UIMarker marker : compass_markers)
+            marker.draw(batch);
     }
 
     public void dispose() {
@@ -78,5 +91,20 @@ public class UIManager {
         health.getTexture().dispose();
         shield.getTexture().dispose();
         back.dispose();
+
+        for (UIMarker marker : compass_markers)
+            marker.dispose();
+    }
+
+    public UIMarker addMarker(Vector2 point) {
+        UIMarker marker = new UIMarker(
+                compass_marker_texture,
+                camera, compass.getWidth() * 0.5f,
+                point
+        );
+
+        compass_markers.add(marker);
+
+        return marker;
     }
 }
