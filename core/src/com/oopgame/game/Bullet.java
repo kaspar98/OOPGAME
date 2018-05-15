14b47200e644cbb23b2831c32f2cc669fe2c5d8e
Kaspar Valk <valk.kaspar@gmail.com>
2018-05-15 18:15:01 +0300

    Bullet die() meetod (pole kindel kas dispose kutsumine vajalik)

--- core/src/com/oopgame/game/Bullet.java
+++ core/src/com/oopgame/game/Bullet.java
@@ -6,23 +6,27 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 
 import helpers.GameInfo;
 
 public class Bullet extends Sprite {
     private Body body;
+    private Fixture fixture;
     private float damage;
+    private BulletManager bm;
 
-    public Bullet (float xKust, float yKust, float xKuhu, float yKuhu, float damage, Texture texture, World world) {
+    public Bullet (float xKust, float yKust, float xKuhu, float yKuhu, float damage, Texture texture, World world, BulletManager bm) {
         super(texture);
         this.damage = damage;
+        this.bm = bm;
         setSize(
                 getTexture().getWidth() * GameInfo.SCALING,
                 getTexture().getHeight() * GameInfo.SCALING
         );
         setOrigin(getWidth() / 2f, getHeight() / 2f);
-        setCenter(xKust, yKust);
 
         BodyDef bodyDef = new BodyDef();
         bodyDef.type = BodyDef.BodyType.DynamicBody;
@@ -31,10 +35,18 @@ public class Bullet extends Sprite {
         // loome bulletile keha
         body = world.createBody(bodyDef);
         body.setUserData(this);
+        body.setBullet(true);
+        PolygonShape box = new PolygonShape();
+        box.setAsBox(getWidth(), getHeight());
+        fixture = body.createFixture(box, 0);
+        fixture.setSensor(true);
+        fixture.setUserData(this);
+
+        box.dispose();
 
         body.applyForceToCenter(new Vector2((xKuhu-xKust)*GameInfo.FORCE_MULTIPLIER, (yKuhu-yKust)*GameInfo.FORCE_MULTIPLIER), true);
-
         setRotation(new Vector2(xKuhu-xKust, yKuhu-yKust).angle()+90);
+        body.setTransform(body.getPosition(), new Vector2(xKuhu-xKust, yKuhu-yKust).angle());
     }
 
     public void draw(Batch batch) {
@@ -47,5 +59,9 @@ public class Bullet extends Sprite {
 
     public void dispose() {
         getTexture().dispose();
+    }
+    public void die() {
+        bm.removeBullet(this);
+        dispose();
     }
 }
