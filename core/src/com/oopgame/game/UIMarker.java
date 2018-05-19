b32c05f38dcac8846a9397c3d9e4344ed717b4c0
Oliver <o.vainumae@gmail.com>
2018-05-19 03:15:23 +0300

    muutsin struktuuri veits

--- core/src/com/oopgame/game/UIMarker.java
+++ core/src/com/oopgame/game/UIMarker.java
@@ -1,50 +1,33 @@
 package com.oopgame.game;
 
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
-
-import helpers.GameInfo;
+import com.badlogic.gdx.math.Vector3;
 
 public class UIMarker extends Sprite {
-    OrthographicCamera camera;
+    private Vector3 camPos;
 
-    Vector2 point;
+    private Vector2 enemyPos;
 
-    float radius;
+    private float radius;
 
-    public UIMarker(Texture texture, OrthographicCamera camera, float radius, Vector2 point) {
-        super(texture);
+    public UIMarker(Sprite appearance, OrthographicCamera camera, float radius, Vector2 enemyPos) {
+        super(appearance);
 
-        this.camera = camera;
+        this.camPos = camera.position;
         this.radius = radius;
-        this.point = point;
-
-        setSize(getWidth() * GameInfo.CAM_SCALING,
-                getHeight() * GameInfo.CAM_SCALING);
-
-        setOrigin(getWidth(), getHeight() * 0.5f);
+        this.enemyPos = enemyPos;
     }
 
     public void update() {
-        float angle = new Vector2(
-                point.x - camera.position.x,
-                point.y - camera.position.y
-        ).angle();
+        float angle = new Vector2(enemyPos.x - camPos.x, enemyPos.y - camPos.y).angle();
+
         setOriginBasedPosition(
-                camera.position.x + MathUtils.cosDeg(angle) * radius,
-                camera.position.y + MathUtils.sinDeg(angle) * radius);
+                camPos.x + MathUtils.cosDeg(angle) * radius,
+                camPos.y + MathUtils.sinDeg(angle) * radius);
 
         setRotation(angle);
-    }
-
-    public Vector2 getPoint() {
-        return point;
-    }
-
-    public void dispose() {
-        getTexture().dispose();
     }
 }
