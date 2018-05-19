477a73b1a268d68581b312d7eaca665cb11ab116
Oliver <o.vainumae@gmail.com>
2018-05-19 03:25:46 +0300

    uusAsukoht veits parandatud

--- core/src/com/oopgame/game/DustParticleManager.java
+++ core/src/com/oopgame/game/DustParticleManager.java
@@ -20,47 +20,44 @@ public class DustParticleManager {
     private Vector2 pos;
     private Vector2 vel;
 
-    private float spawnDistance = (GameInfo.OUTER_RADIUS + 16) * GameInfo.SCALING;
-    private float maxDistance = spawnDistance + 32 * GameInfo.SCALING;
+    private float spawnDistance = (GameInfo.OUTER_RADIUS + 16) * GameInfo.CAM_SCALING;
+    private float maxDistance = spawnDistance + 32 * GameInfo.CAM_SCALING;
 
 
     public DustParticleManager(SpriteBatch batch, Player player) {
         this.batch = batch;
 
-        this.pos = player.body.getPosition();
-        this.vel = player.body.getLinearVelocity();
+        this.pos = player.getPosition();
+        this.vel = player.getVector();
 
         tekstuur = new Texture(Gdx.files.internal("bgl_motiondust_1_t.png"));
 
-        while (tolm.size < 10)
-            tolm.add(new DustParticle(tekstuur, uusAsukoht(true)));
+        while (tolm.size < 20)
+            tolm.add(new DustParticle(tekstuur,
+                    uusAsukoht(true, true)));
     }
 
     public void update() {
-        while (tolm.size < 10)
-            tolm.add(new DustParticle(tekstuur, uusAsukoht(false)));
-
         for (DustParticle tükk : tolm) {
             tükk.update(vel);
 
-            if (new Vector2(
-                    tükk.getX() - pos.x,
-                    tükk.getY() - pos.y
-            ).len() > maxDistance) {
-                tükk.setPosition(uusAsukoht(false));
-            }
+            if (tükk.getPosition().sub(pos).len() > maxDistance)
+                tükk.setPosition(uusAsukoht(false, false));
         }
     }
 
-    private Vector2 uusAsukoht(boolean suvalineKaugus) {
-        float suvaline = MathUtils.degreesToRadians * (MathUtils.random(-45f, 45f) + vel.angle());
+    private Vector2 uusAsukoht(boolean suvalineKaugus, boolean suvalineNurk) {
+        float suvaline = (suvalineNurk ?
+                MathUtils.random(0, 360f) :
+                MathUtils.random(-45f, 45f) + vel.angle());
 
-        float d = (suvalineKaugus ? MathUtils.random(spawnDistance) : spawnDistance);
+        float d = (suvalineKaugus ?
+                MathUtils.random(spawnDistance) :
+                spawnDistance);
 
         return new Vector2(
-                MathUtils.cos(suvaline) * d + pos.x,
-                MathUtils.sin(suvaline) * d + pos.y
-        );
+                MathUtils.cosDeg(suvaline) * d + pos.x,
+                MathUtils.sinDeg(suvaline) * d + pos.y);
     }
 
     public void render() {
