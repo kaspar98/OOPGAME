16cef4c6a508944a22a48c9b3dea84407e54d22e
Oliver <o.vainumae@gmail.com>
2018-08-21 01:24:05 +0300

    puldi controllid

---
+++ core/src/com/oopgame/game/inputs/Pult.java
@@ -0,0 +1,41 @@
+package com.oopgame.game.inputs;
+
+import com.badlogic.gdx.controllers.Controller;
+import com.badlogic.gdx.controllers.Controllers;
+import com.badlogic.gdx.math.Vector2;
+
+public class Pult {
+    public static void movement(Vector2 vector) {
+        Vector2 neo;
+
+        for (Controller controller : Controllers.getControllers()) {
+            neo = new Vector2(controller.getAxis(1), -controller.getAxis(0));
+
+            if (neo.len() > 0.1f)
+                vector.add(neo);
+        }
+    }
+
+    public static boolean aiming(Vector2 vector) {
+        Vector2 neo;
+
+        for (Controller controller : Controllers.getControllers()) {
+            neo = new Vector2(controller.getAxis(3), -controller.getAxis(2));
+
+            if (neo.len() > 0.4f) {
+                vector.set(neo);
+
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isShooting() {
+        for (Controller controller : Controllers.getControllers())
+            if (controller.getButton(5) || controller.getButton(4))
+                return true;
+
+        return false;
+    }
+}
