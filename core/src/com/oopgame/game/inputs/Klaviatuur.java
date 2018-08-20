c53aff3a0f9267ede71c4347f8f4fc9beb254458
Oliver <o.vainumae@gmail.com>
2018-08-21 01:24:59 +0300

    klaviatuuri controllid

---
+++ core/src/com/oopgame/game/inputs/Klaviatuur.java
@@ -0,0 +1,21 @@
+package com.oopgame.game.inputs;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.math.Vector2;
+
+public class Klaviatuur {
+    public static void movement(Vector2 vector) {
+        if (Gdx.input.isKeyPressed(Input.Keys.W))
+            vector.add(0, 1);
+
+        if (Gdx.input.isKeyPressed(Input.Keys.A))
+            vector.add(-1, 0);
+
+        if (Gdx.input.isKeyPressed(Input.Keys.S))
+            vector.add(0, -1);
+
+        if (Gdx.input.isKeyPressed(Input.Keys.D))
+            vector.add(1, 0);
+    }
+}
