36fd596094b2ab6032a29771418767f3e4baa6f8
Oliver <o.vainumae@gmail.com>
2018-08-26 01:14:01 +0300

    lisasin boolean FULLSCREEN'i ja eemaldasin BulletManageri jaoks vajalikud väljad

--- core/src/helpers/GameInfo.java
+++ core/src/helpers/GameInfo.java
@@ -3,8 +3,9 @@ package helpers;
 import com.badlogic.gdx.math.Vector2;
 
 public class GameInfo {
-    public static final int WIDTH = 1920;
-    public static final int HEIGHT = 1080;
+    public static final int WIDTH = 1600;
+    public static final int HEIGHT = 900;
+    public static final boolean FULLSCREEN = false;
 
     public static final float INNER_RADIUS = (WIDTH < HEIGHT ? WIDTH : HEIGHT);
     public static final float OUTER_RADIUS = new Vector2(WIDTH, HEIGHT).len() / 2f;
@@ -16,15 +17,12 @@ public class GameInfo {
     public static final float W_HEIGHT = 2048;
 
     public static final float FORCE_MULTIPLIER = 5000;
+
     public static final float PLAYER_MAXSPEED = 45;
     public static final float PLAYER_ACCELERATION = 0.25f;
-    public static final long PLAYER_SHOOTING_INTERVAL = 200;
     public static final long PLAYER_DAMAGED_DURATION = 200;
 
-    public static final long ENEMY_SHOOTING_INTERVAL = 1000;
     public static final long ENEMY_DAMAGED_DURATION = 200;
 
     public static final long GIBS_DURATION = 5000;
-
-    public static final long BULLET_DURATION = 2500;
 }
