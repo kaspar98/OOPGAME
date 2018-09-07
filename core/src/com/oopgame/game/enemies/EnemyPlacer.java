0664c592b2cfa52973447e0b745b66b15a9d88b3
Oliver <o.vainumae@gmail.com>
2018-09-07 17:47:06 +0300

    klass, mille objektid paigutavad Mothershipi ette antud delay-ga

---
+++ core/src/com/oopgame/game/enemies/EnemyPlacer.java
@@ -0,0 +1,79 @@
+package com.oopgame.game.enemies;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
+import com.oopgame.game.Time;
+import com.oopgame.game.enemies.ships.MotherShip1;
+import com.oopgame.game.vfx.VisualEffectsManager;
+
+import helpers.GameInfo;
+
+public class EnemyPlacer {
+    private Vector2 pos = new Vector2();
+    private float angle;
+
+    private Time time;
+    private long spawnTime;
+    private int spawnInterval;
+
+    private String carrierKey;
+
+    private int[] enemyCounts = new int[1];
+    // 0 : fastEnemies
+
+    private EnemyManager enemyManager;
+    private VisualEffectsManager vfxManager;
+
+    public EnemyPlacer(float x, float y, float angle, String carrierKey,
+                       Time time, int millisTillSpawn, int millisSpawnInterval,
+                       int fastEnemies,
+                       EnemyManager enemyManager, VisualEffectsManager vfxManager) {
+        this.time = time;
+
+        this.enemyManager = enemyManager;
+        this.vfxManager = vfxManager;
+
+        reconfigure(x, y, angle, carrierKey,
+                millisTillSpawn, millisSpawnInterval,
+                fastEnemies);
+    }
+
+    public void reconfigure(float x, float y, float angle, String carrierKey,
+                            int millisTillSpawn, int millisSpawnInterval,
+                            int fastEnemies) {
+        pos.set(x, y);
+        this.angle = angle;
+
+        this.carrierKey = carrierKey;
+
+        spawnTime = time.getTime() + millisTillSpawn;
+        this.spawnInterval = millisSpawnInterval;
+
+        enemyCounts[0] = fastEnemies;
+
+        int framesTillSpawn = MathUtils.round(
+                millisTillSpawn / (1000f / GameInfo.FPS));
+
+        float midpoint = (framesTillSpawn > 0 ?
+                (float) framesTillSpawn / (framesTillSpawn + 300) :
+                0);
+
+        vfxManager.addPortal(1, x, y, 10, framesTillSpawn + 300,
+                midpoint, 0.5f);
+    }
+
+    public void update() {
+        if (time.getTime() > spawnTime) {
+            if (MotherShip1.keyType.equals(carrierKey))
+                enemyManager.addMotherShip1(pos.x, pos.y, angle, spawnInterval,
+                        enemyCounts[0]);
+
+            vfxManager.addBloom(2, pos.x, pos.y, 10,
+                    Color.WHITE, 10, 0.5f, 0.5f, 0,
+                    0, 0);
+
+            enemyManager.poolEnemyPlacer(this);
+        }
+    }
+}
