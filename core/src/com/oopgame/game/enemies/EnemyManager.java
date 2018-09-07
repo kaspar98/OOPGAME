bc4197935886849b4dd58b5e9306a5399b77b768
Oliver <o.vainumae@gmail.com>
2018-09-07 18:01:14 +0300

    vastaste loomiseks vajalikud objektid pannakse valmis alles täpselt enne esimese seda tüüpi vastase spawnimist

--- core/src/com/oopgame/game/enemies/EnemyManager.java
+++ core/src/com/oopgame/game/enemies/EnemyManager.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.CircleShape;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
@@ -18,6 +19,7 @@ import com.oopgame.game.enemies.ai.EnemyAI;
 import com.oopgame.game.enemies.ai.RegularEnemy;
 import com.oopgame.game.enemies.ships.EnemyShip;
 import com.oopgame.game.enemies.ships.FastShip;
+import com.oopgame.game.enemies.ships.MotherShip1;
 import com.oopgame.game.guns.damagers.DamagerManager;
 import com.oopgame.game.vfx.VisualEffectsManager;
 
@@ -42,12 +44,7 @@ public class EnemyManager {
     private VisualEffectsManager vfxManager;
 
     // sprite'ide hoidmiseks
-    private Map<String, Sprite> spriteMap = new HashMap<String, Sprite>();
-
-    private List<EnemyShip> aliveShips = new ArrayList<EnemyShip>();
-    private Map<String, Deque<EnemyShip>> shipPools = new HashMap<String, Deque<EnemyShip>>();
-    private Deque<EnemyShip> toDeactivate = new LinkedList<EnemyShip>();
-
+    private Map<String, List<Sprite>> spriteMap = new HashMap<String, List<Sprite>>();
 
     private Map<String, BodyDef> bodyDefMap = new HashMap<String, BodyDef>();
 
@@ -55,9 +52,18 @@ public class EnemyManager {
 
     private Map<String, Shape> shapeMap = new HashMap<String, Shape>();
 
-
     // ai map
     private Map<String, EnemyAI> aiMap = new HashMap<String, EnemyAI>();
+
+    private Deque<EnemyShip> toAdd = new LinkedList<EnemyShip>();
+
+    private List<EnemyShip> aliveShips = new ArrayList<EnemyShip>();
+    private Map<String, Deque<EnemyShip>> shipPools = new HashMap<String, Deque<EnemyShip>>();
+    private Deque<EnemyShip> toDeactivate = new LinkedList<EnemyShip>();
+
+    private List<EnemyPlacer> placersAlive = new ArrayList<EnemyPlacer>();
+    private Deque<EnemyPlacer> toDeactivatePlacers = new LinkedList<EnemyPlacer>();
+    private Deque<EnemyPlacer> placersPool = new LinkedList<EnemyPlacer>();
 
 
     public EnemyManager(SpriteBatch batch, World world, Time time, Player player,
@@ -73,69 +79,48 @@ public class EnemyManager {
         // TODO: vb saab seda mappide valmispanemise protsessi vastaste loomise juurde tõsta?
 
         // paneme valmis FastShip objektide loomiseks vajalikud objektid
-        String key = "fastShip";
 
-        Sprite sprite = new Sprite(new Texture(
-                Gdx.files.internal("ships/enemy_alien_fighter_1b_t.png")));
-
-        sprite.setSize(
-                sprite.getWidth() * GameInfo.SCALING,
-                sprite.getHeight() * GameInfo.SCALING);
-
-        sprite.setOrigin(
-                sprite.getWidth() * 0.5f,
-                sprite.getHeight() * 0.5f);
-
-        spriteMap.put(key, sprite);
-
-        BodyDef bodyDef = new BodyDef();
-        bodyDef.type = BodyDef.BodyType.DynamicBody;
-
-        bodyDefMap.put(key, bodyDef);
-
-        CircleShape circle = new CircleShape();
-        circle.setRadius(spriteMap.get(key).getHeight() * 0.3f);
-
-        shapeMap.put(key, circle);
-
-        FixtureDef fixtureDef = new FixtureDef();
-        fixtureDef.shape = shapeMap.get(key);
-        fixtureDef.density = 0.9f;
-        fixtureDef.friction = 0.5f;
-        fixtureDef.restitution = 0.1f;
-
-        fixtureDefMap.put(key, fixtureDef);
-
-
-        aiMap.put("regular", new RegularEnemy(player));
+        aiMap.put(RegularEnemy.keyType, new RegularEnemy(player));
     }
 
     public void update() {
+        for (EnemyPlacer placer = toDeactivatePlacers.poll(); placer != null;
+             placer = toDeactivatePlacers.poll()) {
+            placersAlive.remove(placer);
+            placersPool.add(placer);
+        }
+
         for (EnemyShip ship = toDeactivate.poll(); ship != null; ship = toDeactivate.poll()) {
             aliveShips.remove(ship);
 
-            String key;
+            String key = ship.getKeyType();
 
-            if (ship instanceof FastShip) {
-                key = "fastShip";
-            } else
-                throw new RuntimeException("sellist vastase laeva tüüpi pole siin välja toodud!");
+            Body shipBody = ship.getBody();
+            float scale = 1;
+            if (ship instanceof MotherShip1)
+                scale = 2;
+
+            vfxManager.addExplosion(2,
+                    shipBody.getPosition().x,
+                    shipBody.getPosition().y,
+                    scale, Color.WHITE);
+
+            ship.deactivate();
 
             if (!shipPools.containsKey(key))
                 shipPools.put(key, new LinkedList<EnemyShip>());
 
-            vfxManager.addExplosion(2,
-                    ship.getBody().getPosition().x,
-                    ship.getBody().getPosition().y,
-                    1, Color.WHITE);
-
-            ship.deactivate();
-
             shipPools.get(key).add(ship);
         }
 
+        for (EnemyShip ship = toAdd.poll(); ship != null; ship = toAdd.poll())
+            aliveShips.add(ship);
+
         for (EnemyShip ship : aliveShips)
             ship.update();
+
+        for (EnemyPlacer placer : placersAlive)
+            placer.update();
     }
 
     public void render() {
@@ -144,46 +129,165 @@ public class EnemyManager {
     }
 
     public void dispose() {
-        for (Sprite sprite : spriteMap.values())
-            sprite.getTexture().dispose();
+        for (List<Sprite> sprites : spriteMap.values())
+            for (Sprite sprite : sprites)
+                sprite.getTexture().dispose();
 
         for (Shape shape : shapeMap.values())
             shape.dispose();
     }
 
-    public void addEnemy(int fastShipCount) {
-        if (fastShipCount <= 0) return;
+    public void addEnemyPlacer(float x, float y, float angle, String carrierKey,
+                               int millisTillSpawn, int millisSpawnInterval,
+                               int fastEnemies) {
+        EnemyPlacer placer = placersPool.poll();
 
-        // TODO: testimisega leida hea kaugus, kus vastaseid spawnida
-        Vector2 spawn = playerPos.cpy().add(
-                new Vector2(1, 0).setLength(GameInfo.OUTER_RADIUS * GameInfo.SCALING)
-                        .setAngle(MathUtils.random(360)));
+        if (placer != null)
+            placer.reconfigure(x, y, angle, carrierKey,
+                    millisTillSpawn, millisSpawnInterval,
+                    fastEnemies);
+        else
+            placer = new EnemyPlacer(x, y, angle, carrierKey,
+                    time, millisTillSpawn, millisSpawnInterval,
+                    fastEnemies,
+                    this, vfxManager);
 
-        // TODO: tuleb natuke veel ümber teha, et teise klassi laevu ka spawniks
-        for (int i = 0; i < fastShipCount; i++) {
-            Vector2 point = spawn.cpy().add(new Vector2(1, 0).setLength(100 * GameInfo.SCALING)
-                    .setAngle(MathUtils.random(360)));
+        placersAlive.add(placer);
+    }
 
-            String key = "fastShip";
+    public void addMotherShip1(float x, float y, float angle,
+                               int millisSpawnInterval,
+                               int fastShips) {
+        String key = MotherShip1.keyType;
 
-            if (!shipPools.containsKey(key))
-                shipPools.put(key, new LinkedList<EnemyShip>());
+        if (!spriteMap.containsKey(key)) {
+            List<Sprite> sprites = new ArrayList<Sprite>();
 
-            EnemyShip ship = shipPools.get(key).poll();
+            Sprite sprite = new Sprite(new Texture(
+                    Gdx.files.internal("ships/enemy_alien_fighter_1_t.png")));
 
-            if (ship != null) {
-                ship.reset(point);
-            } else {
-                ship = new FastShip(point, world, spriteMap.get(key), time,
-                        bodyDefMap.get(key), fixtureDefMap.get(key),
-                        this, damagerManager, aiMap.get("regular"));
-            }
+            sprite.setScale(GameInfo.SCALING * 2.5f);
 
-            aliveShips.add(ship);
+            sprite.setOrigin(
+                    sprite.getWidth() * 0.5f,
+                    sprite.getHeight() * 0.5f);
+
+            sprites.add(sprite);
+            spriteMap.put(key, sprites);
+
+            BodyDef bodyDef = new BodyDef();
+            bodyDef.type = BodyDef.BodyType.DynamicBody;
+
+            bodyDefMap.put(key, bodyDef);
+
+            CircleShape circle = new CircleShape();
+            circle.setRadius(spriteMap.get(key).get(0).getHeight() * 0.09f);
+
+            shapeMap.put(key, circle);
+
+            FixtureDef fixtureDef = new FixtureDef();
+            fixtureDef.shape = shapeMap.get(key);
+            fixtureDef.density = 0.9f;
+            fixtureDef.friction = 0.5f;
+            fixtureDef.restitution = 0.1f;
+
+            fixtureDefMap.put(key, fixtureDef);
         }
+
+        MotherShip1 carrier;
+
+        EnemyShip ship = getFromPool(key);
+
+        if (ship != null) {
+            carrier = (MotherShip1) ship;
+            carrier.reconfigure(x, y, angle, null, millisSpawnInterval,
+                    fastShips);
+            carrier.reset();
+        } else {
+            carrier = new MotherShip1(x, y, angle, world,
+                    time, millisSpawnInterval,
+                    fastShips,
+                    spriteMap.get(key), bodyDefMap.get(key), fixtureDefMap.get(key),
+                    this, damagerManager, vfxManager);
+        }
+
+        toAdd.add(carrier);
+    }
+
+    public void addFastShip(float x, float y, float angle) {
+        String key = FastShip.keyType;
+
+        if (!spriteMap.containsKey(key)) {
+            List<Sprite> sprites = new ArrayList<Sprite>();
+
+            Sprite sprite = new Sprite(new Texture(
+                    Gdx.files.internal("ships/enemy_alien_fighter_1b_t.png")));
+
+            sprite.setScale(GameInfo.SCALING);
+
+            sprite.setOrigin(
+                    sprite.getWidth() * 0.5f,
+                    sprite.getHeight() * 0.5f);
+            sprites.add(sprite);
+            spriteMap.put(key, sprites);
+
+            BodyDef bodyDef = new BodyDef();
+            bodyDef.type = BodyDef.BodyType.DynamicBody;
+
+            bodyDefMap.put(key, bodyDef);
+
+            CircleShape circle = new CircleShape();
+            circle.setRadius(spriteMap.get(key).get(0).getHeight() * 0.04f);
+
+            shapeMap.put(key, circle);
+
+            FixtureDef fixtureDef = new FixtureDef();
+            fixtureDef.shape = shapeMap.get(key);
+            fixtureDef.density = 0.9f;
+            fixtureDef.friction = 0.5f;
+            fixtureDef.restitution = 0.1f;
+
+            fixtureDefMap.put(key, fixtureDef);
+        }
+
+        FastShip fastShip;
+        EnemyShip ship = getFromPool(key);
+
+        if (ship != null) {
+            fastShip = (FastShip) ship;
+            fastShip.reconfigure(x, y, angle, aiMap.get("regular"));
+            fastShip.reset();
+        } else {
+            fastShip = new FastShip(x, y, angle, world, time,
+                    spriteMap.get(key), bodyDefMap.get(key), fixtureDefMap.get(key),
+                    this, damagerManager, aiMap.get("regular"));
+        }
+
+        toAdd.add(fastShip);
+    }
+
+    public Vector2 posNearPlayer(float radius) {
+        return posNearPlayer(radius, new Vector2());
+    }
+
+    public Vector2 posNearPlayer(float radius, Vector2 vector) {
+        return vector.set(1, 0).setLength(radius)
+                .setAngle(MathUtils.random(360))
+                .add(playerPos);
+    }
+
+    private EnemyShip getFromPool(String keyType) {
+        if (!shipPools.containsKey(keyType))
+            shipPools.put(keyType, new LinkedList<EnemyShip>());
+
+        return shipPools.get(keyType).poll();
     }
 
     public void poolEnemyShip(EnemyShip ship) {
         toDeactivate.add(ship);
+    }
+
+    public void poolEnemyPlacer(EnemyPlacer enemyPlacer) {
+        toDeactivatePlacers.add(enemyPlacer);
     }
 }
